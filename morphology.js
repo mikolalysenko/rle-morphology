85cc2196af60ff3e19350087520fc3c03022af47
Mikola Lysenko <mikolalysenko@gmail.com>
2013-02-02 15:43:17 -0600

    seems to work

--- morphology.js
+++ morphology.js
@@ -1,30 +1,35 @@
-var apply = require("rle-core").apply;
+var apply = require("rle-funcs").apply;
 
-var DILATE_FUNC = new Function("v", [
-  "var r = v[0];",
-  "for(var i=1; i<v.length; ++i) {",
-    "r = Math.min(r, v);",
+var DILATE_FUNC = new Function("p", "d", "retval", [
+  "var s = p[0];",
+  "var r = p[0] ? -d[0] : d[0];",
+  "for(var i=1; i<p.length; ++i) {",
+    "r = Math.min(r, p[i] ? -d[i] : d[i]);",
+    "s = s | p[i];",
   "}",
-  "return r;"
+  "retval[0] = s;",
+  "retval[1] = Math.abs(r);"
 ].join("\n"));
 
 function dilate(volume, stencil) {
   return apply(volume, stencil, DILATE_FUNC);
 }
 
-var ERODE_FUNC = new Function("v", [
-  "var r = v[0];",
-  "for(var i=1; i<v.length; ++i) {",
-    "r = Math.max(r, v);",
+var ERODE_FUNC = new Function("p", "d", "retval", [
+  "var s = p[0];",
+  "var r = p[0] ? -d[0] : d[0];",
+  "for(var i=1; i<p.length; ++i) {",
+    "r = Math.max(r, p[i] ? -d[i] : d[i]);",
+    "s = s & p[i];",
   "}",
-  "return r;"
+  "retval[0] = s;",
+  "retval[1] = Math.abs(r);"
 ].join("\n"));
 
 function erode(volume, stencil) {
   return apply(volume, stencil, ERODE_FUNC);
 }
 
-exports.apply   = apply;
 exports.dilate  = dilate;
 exports.erode   = erode
 exports.closing = function(volume, stencil) { return erode(dilate(volume, stencil), stencil); }
